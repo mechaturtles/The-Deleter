e84b353c8483ed4f8cbd3f12ef7bb2f4175d5b2a
Bryce Villanueva <bryce.christian@gmail.com>
2019-06-04 14:13:19 -0500

    Add files via upload

--- deleter.py
+++ deleter.py
@@ -1,64 +1,87 @@
 import os
 import sys
 
-#needed functions: prefix
-# addpath = "Unknown folder/"
+# needed functions: prefix, zero result handling
 
-#File delete function with fault exception
+# File delete function with fault exception
 def delete(file):
     try:
         print("Deleting " + file)
+        bsize = os.path.getsize(addpath + file) #Size in bytes
         os.remove(addpath + file)
+        return bsize
     except:
         print("Error: File" + file + " is missing. Moving on.")
+        return 0
 
-def filesize(file):
-    size = os.path.getsize(addpath + file)
+# Determines the right unit for a filesize and calculates for the unit.
+# Output is an array.
+def filesize(bsize):
 # Decide what unit to use
-    if size >= 1024**3:
-        size = str(round(size / 1024**3, 3))
+    if bsize >= 1024**3:
+        size = str(round(bsize / 1024**3, 3))
         unit = "GB"
-    elif size >= 1024**2:
-        size = str(round(size / 1024**2, 3))
+    elif bsize >= 1024**2:
+        size = str(round(bsize / 1024**2, 3))
         unit = "MB"
-    elif size >= 1024:
-        size = str(round(size / 1024, 3))
+    elif bsize >= 1024:
+        size = str(round(bsize / 1024, 3))
         unit = "KB"
     else:
-        size = str(size / 1024) + " B"
-    return size
+        size = str(bsize)
+        unit = "B"
+# Build and return array
+    array = [size, unit]
+    return array
 
 
+print("The Deleter\nby Bryce Villanueva\n")
 addpath = "Unknown folder/"
 path = os.path.dirname(sys.argv[0]) + "/" + addpath
-print(path)
-dir = os.listdir(path)
+print("Accessing " + path)
+
+
+sess_total = 0 # Total of files deleted for the session
 
 ans = "y"
 while ans == "y": # Repeat until ans is valid
+    dir = os.listdir(path)
     suffix = input("\nWhat extension are you looking for?\n")
 
     print("\nSearching for files ending in \"" + suffix + "\"...")
     list = []
+
     for file in dir:
         if file.endswith(suffix):
             list.append(file)
 
-    dtotal = 0
+    # Declare/reset running totals
+    select_total = 0 # Files selected
+    del_total = 0 # Files deleted
     for file in list:
-        fsize = filesize(file) #Filesize value is obtained and turned into int value
-        print("{0:50}      {1:10}".format(file, fsize))
+        bsize = os.path.getsize(addpath + file) # Size in bytes
+        fsize = filesize(bsize) # Filesize value is obtained and turned into int value
+        print("{0:50}     {1:10} {2:3}".format(file, fsize[0], fsize[1]))
+        select_total += bsize
 
-    if input("\nWould you like to delete all files?\n") == "y":
+    select_size = filesize(select_total)
+
+    if select_total == 0:
+        print("\nNo files found.")
+    elif input("\nWould you like to delete all files? {} {} selected. (y/n)\n".format(select_size[0], select_size[1])) == "y":
         for file in list:
-            delete(file) #Call on delete function
-        print("\nDeletion complete.")
+            bsize = delete(file)
+            if bsize != 0: # Call on delete function
+                del_total += bsize
+        del_size = filesize(del_total)
+        print("\nDeletion complete. Deleted {} {} out of {} {} selected.".format(del_size[0], del_size[1], select_size[0], select_size[1]))
+
 
     while True:
-        ans = input("Restart? (y/n)\n")
+        ans = input("\nRestart? (y/n)\n")
         if ans == "n" or ans == "y":
             break
         else:
             print("Invalid input.\n")
 
-ans = input("\nProgram finished.\n")
+ans = input("\nProgram finished. Press enter to exit.\n")
